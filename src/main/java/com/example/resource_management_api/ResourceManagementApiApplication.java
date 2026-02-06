e8f4a95b534690fc1ebd60dbf5225d8d49f49f5b
leeminho <twomean99@gmail.com>
2026-02-06 14:23:26 +0900

    feat: add base entity with auditing

--- src/main/java/com/example/resource_management_api/ResourceManagementApiApplication.java
+++ src/main/java/com/example/resource_management_api/ResourceManagementApiApplication.java
@@ -2,8 +2,10 @@ package com.example.resource_management_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ResourceManagementApiApplication {
 
 	public static void main(String[] args) {

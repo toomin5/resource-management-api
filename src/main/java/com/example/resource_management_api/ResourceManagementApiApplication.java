a239c55b2d2f7c1426ea5c467547b5ca5f469266
leeminho <twomean99@gmail.com>
2026-02-06 14:02:03 +0900

    chore: init project

---
+++ src/main/java/com/example/resource_management_api/ResourceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.resource_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagementApiApplication.class, args);
+	}
+
+}

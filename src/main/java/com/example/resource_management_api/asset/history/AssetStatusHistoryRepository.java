5c757866681770922914d3ecfae76f1a12e42d13
leeminho <twomean99@gmail.com>
2026-02-09 01:51:13 +0900

    docs: update API description

--- src/main/java/com/example/resource_management_api/asset/history/AssetStatusHistoryRepository.java
+++ src/main/java/com/example/resource_management_api/asset/history/AssetStatusHistoryRepository.java
@@ -2,5 +2,10 @@ package com.example.resource_management_api.asset.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AssetStatusHistoryRepository extends JpaRepository<AssetStatusHistory, Long> {
+import java.util.List;
+
+public interface AssetStatusHistoryRepository
+        extends JpaRepository<AssetStatusHistory, Long> {
+
+    List<AssetStatusHistory> findByAssetIdOrderByChangedAtDesc(Long assetId);
 }
